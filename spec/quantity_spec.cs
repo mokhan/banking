87e76020386c480c88c8ea6f44890376a66b1498
mo <mokha@cisco.com>
2017-04-22 07:48:07 -0600

    convert all currency to CAD.

--- spec/quantity_spec.cs
+++ spec/quantity_spec.cs
@@ -24,3 +24,27 @@ public class QuantitySpec
     Assert.NotEqual(new Quantity(100, Currency.CAD), new Quantity(100, Currency.USD));
   }
 }
+
+public class CurrencySpec
+{
+  [Fact]
+  public void converts_USD_to_CAD()
+  {
+    var result = Currency.CAD.convert(0.50, Currency.USD);
+    Assert.Equal(1, result);
+  }
+
+  [Fact]
+  public void converts_MXN_to_CAD()
+  {
+    var result = Currency.CAD.convert(10.0, Currency.MXN);
+    Assert.Equal(1.0, result);
+  }
+
+  [Fact]
+  public void converts_CAD_to_CAD()
+  {
+    var result = Currency.CAD.convert(10.0, Currency.CAD);
+    Assert.Equal(10.0, result);
+  }
+}
